cfce346ede9475446c57501d22dd5b04d7972a4e
rxo1765 <136536690+rxo1765@users.noreply.github.com>
2023-07-25 17:29:11 -0700

    LRoverUnder.py

--- LRoverUnder.py
+++ LRoverUnder.py
@@ -17,7 +17,8 @@ import numpy as np
 # look at column names, and I want to plot betttin line versus what actually 
 # happened
 
-NFLdata = pd.read_csv('NFL2022')
+NFLdata = pd.read_csv('NFL2022.csv')
+n = len(NFLdata)
 type(NFLdata)
 names = list(NFLdata)
 print(names)
@@ -33,36 +34,44 @@ print(b0, b1)
 m_x = np.mean(x)
 std_x = np.std(x)
 
-histAx = np.linspace(10,90)
+histAx = np.linspace(10,90,90)
 plt.hist(x, alpha = 0.35, label='line', bins=20)
 plt.hist(y, alpha = 0.45, label = 'actual spread', bins=20)
 plt.xlabel('points')
-plt.ylabel('frequency')
+plt.ylabel('probability')
+plt.plot(histAx, norm.pdf(histAx, m_x, std_x), "r-")
 plt.legend()
 plt.grid()
 plt.show()
 
 plt.plot(x, y, 'o')
-xAxis = np.linspace(-20, 15)
+xAxis = np.linspace(30, 60)
 
 yPred = b0+b1*xAxis
 plt.plot(xAxis, yPred, color='red')
 plt.xlabel('line')
 plt.ylabel('home score-visit score')
-plt.hlines(y=0, xmin=30, xmax=60, color='orange')
+plt.plot(xAxis, xAxis, color='orange')
 plt.grid()
 
 bank = 0
 winCount = 0
 loseCount = 0
 for i in range (n):
-  if x[i] > 53.5:
-    if y[i] > x[i]:
-      bank += 300
-      winCount += 1
-    else:
-      bank += 330
-      loseCount += 1
+    if x[i] > 53.5:
+        if y[i] > x[i]: 
+            bank += 300
+            winCount +=1
+        else:
+            bank += 330
+            loseCount += 1
     if x[i] < 32.5:
-      if y[i] < x[i]:
-        bank +=300
+        if y[i] < x[i]:
+                bank += 300
+                winCount += 1
+        else:
+            bank -=330
+            loseCount +=1
+            
+print(f'bank = {bank}')
+print(f"wins: {winCount}, loses {loseCount}")
