206d41a154520d1c453c3dd9205e3853801f05af
rxo1765 <136536690+rxo1765@users.noreply.github.com>
2023-08-01 17:32:25 -0700

    Update SOE.py

--- SOE.py
+++ SOE.py
@@ -1,9 +1,8 @@
 import numpy as np
-import sys
-
+import sys 
 A = np.array([[2,3,-4],
-              [1,-2,
-              [3,4]])
+              [1,-2,3],
+              [3,-1,-1]])
 
 print(A)
 type(A)
@@ -11,33 +10,35 @@ A.shape
 
 '''
 system of equations:
-  suppose x1 =5, and x2 =1 then
-  1*x1 + 2*x2 = 7
+suppose x1 =5, and x2 =1 then
+  1*x1 + 2*x2 = 9
   3*x1 + 4*x2 = 19
 
 A@x = b
 A.inv @ A @x =  A.inv @ b
 '''
 
-b =np.array([ [7],
-             [19]])
+b =np.array([[-4],
+             [6],
+             [-2]])
+
 print(f"\nb = {b}")
 
 ## solve equations
 
 detA = np.linalg.det(A)
-print (f"determinant of matrix A is {detA: 0.3f}")
+print(f"determinant of matrix A is {detA: 0.3f}")
+if detA == 0:
+    print("/ndet A = 0; no solution\n")
+    sys.exit()
 
 Ainv = np.linalg.inv(A)
-print(f"\nA- inverse = \n{Ainv}")
+print(f"A- inverse = \n{Ainv}")
 
-print (f"\n Ainv @ A is {Ainv @ A}")
+print(Ainv @ A)
 
 x = Ainv @ b
 print(f"\nunknowns are: \n{x}")
 
-# check
+#check
 print(f"\ncheck: \n{A@x}")
-
-
-
